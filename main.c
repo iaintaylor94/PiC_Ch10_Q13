7d03e7911ee495f6ea5e9721caf2cc110281013a
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-02 14:37:56 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
+// Program to convert all characters in a string to their uppercase equivalent
+
 #include <stdio.h>
+#include <string.h>
+
+// Declare Functions
+void uppercase (char []);
 
 int main(void) {
-  printf("Hello World\n");
+
+  char in[] = "Hello World!";
+  
+  printf ("%s\n", in);
+  uppercase (in);
+  printf ("%s\n", in);
+  
   return 0;
+}
+
+// Define Functions
+void uppercase (char input[]) {
+  for (int i = 0; input[i] != '\0'; i++) {
+    if ('a' <= input[i] && input[i] <= 'z')
+      input[i] += 'A' - 'a';
+  }
 }
